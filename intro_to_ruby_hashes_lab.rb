04bc5d6be5aed491dfbf806336ccca53d2acb069
Sara McCombs <thesaramccombs@gmail.com>
2019-08-02 20:37:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,8 +35,8 @@ def monopoly_with_third_tier
   	  :rent_in_dollars => {
   	    :one_piece_owned => 25,
   	    :two_pieces_owned => 50,
-  	    :key3 => 3,
-  	    :key4 => 4
+  	    :three_pieces_owned => 75,
+  	    :four_pieces_owned => 100
   	    }
   	  }
   	}
