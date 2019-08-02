db444e3e72ac730d01545b8cbb4826c1e5edf2c2
Sara McCombs <thesaramccombs@gmail.com>
2019-08-02 20:39:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
   	    :reading_railroad => {},
   	    :pennsylvania_railroad => {},
   	    :b_and_o_railroad => {},
-  	    :short_line_railroad => {} 
+  	    :shortline_railroad => {} 
   	  },
   	  :rent_in_dollars => {
   	    :one_piece_owned => 25,
